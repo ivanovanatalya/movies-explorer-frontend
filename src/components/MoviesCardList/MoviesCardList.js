08b68211937db5fd4fd8882c77ae839e7073785e
Natasha Ivanova <terehinann@gmail.com>
2023-03-22 01:49:03 +0300

    fix:landing 320px

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -2,7 +2,7 @@ import MovieCard from "../MovieCard/MovieCard";
 import "./MoviesCardList.css";
 
 const MoviesCardList = ({ isSaved }) => {
-  const movies = [1,2,3,4,5,6];
+  const movies = [1,2,3,4,5];
   return (
     <section className="card-list">
       {movies.map(() => <MovieCard isSaved />)}
