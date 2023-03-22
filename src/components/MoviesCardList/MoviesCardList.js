b03128104a16f3b8e9054578d8579b300055bfbf
Natasha Ivanova <terehinann@gmail.com>
2023-03-22 12:19:07 +0300

    fix: fix landing saved

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -2,7 +2,7 @@ import MovieCard from "../MovieCard/MovieCard";
 import "./MoviesCardList.css";
 
 const MoviesCardList = ({ isSaved }) => {
-  const movies = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+  const movies = [1,2,3];
   return (
     <section className="card-list">
       {movies.map(() => <MovieCard isSaved />)}
